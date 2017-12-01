e3c3fc1ec4cf6bb8584a7ed6a820b585b6fb29e0
Jerome Ortega <jeromeportega@gmail.com>
2017-12-01 12:10:57 -0800

    Update makeArrayConsecutive2.js

--- CodeFights/AdventureMode/Intro/EdgeOfTheOcean/makeArrayConsecutive2.js
+++ CodeFights/AdventureMode/Intro/EdgeOfTheOcean/makeArrayConsecutive2.js
@@ -1,4 +1,8 @@
-// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+// Richey got statues of different sizes as a present from John for his birthday, 
+// each statue having an non-negative integer size. Since he likes to make things perfect, 
+// he wants to arrange them from smallest to largest so that each statue will be bigger than the 
+// previous one exactly by 1. He may need some additional statues to be able to accomplish that. 
+// Help him figure out the minimum number of additional statues needed.
 
 function makeArrayConsecutive2(statues) {
     var min = statues[0];
@@ -8,8 +12,6 @@ function makeArrayConsecutive2(statues) {
         if (statues[i] < min) min = statues[i];
         if (statues[i] > max) max = statues[i];
     }
-    console.log(min);
-    console.log(max);
     total = max - min - statues.length + 1;
     return total;
 }
